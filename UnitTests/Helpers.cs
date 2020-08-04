fca978771bab5970c21b4a8975740111f79238b9
Christopher Wötzel <aucubin@aucubin.de>
2020-08-04 17:46:16 +0200

    - first unit test for xml parsing - helpers class for unit tests which contain functions to help with unit testing (just function to get external files for unit testing for now)

---
+++ UnitTests/Helpers.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace UnitTests
+{
+    internal class Helpers
+    {
+        internal static string GetAbsolutePathForExternalUnitTestingFile(string fileName)
+        {
+            return Path.Combine(GetPathToCurrentUnitTestingProject(), fileName);
+        }
+
+        private static string GetPathToCurrentUnitTestingProject()
+        {
+            string pathAssembly = Assembly.GetExecutingAssembly().Location;
+            string folderAssembly = Path.GetDirectoryName(pathAssembly);
+            if (!folderAssembly.EndsWith(Path.DirectorySeparatorChar))
+            {
+                folderAssembly += Path.DirectorySeparatorChar;
+            }
+            return Path.GetFullPath(folderAssembly + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar);
+        }
+    }
+}
---
+++ UnitTests/XmlParsing/XmlParserTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using XmlParsing;
+using System.Collections.Generic;
+using InternalRepresentation;
+using System.IO;
+
+namespace UnitTests.XmlParsing
+{
+    public class XmlParserTests
+    {
+        private readonly string externalFileRootPath = "XmlParsing" + Path.DirectorySeparatorChar + "TestFiles" + Path.DirectorySeparatorChar;
+
+        [Fact]
+        public void SimpleXmlTest()
+        {
+            string path = Helpers.GetAbsolutePathForExternalUnitTestingFile(externalFileRootPath+"SimpleXmlTest.xml");
+            HashSet<string> fields = new HashSet<string>() { "test" };
+            XmlParser xml = new XmlParser(path, "child", fields);
+            xml.ParseXML();
+
+
+            Representation expectedRep = new Representation(fields);
+            expectedRep["test"].StringValue = "TestValue";
+
+            Assert.Equal(expectedRep, xml.ParsedXml[0]);
+        }
+    }
+}
