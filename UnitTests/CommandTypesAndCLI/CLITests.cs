1dc8202d70c43af84400ca09d23de14f5296566c
Christopher Wötzel <aucubin@aucubin.de>
2020-08-07 20:35:18 +0200

    - unit tests which call the cli directly and check if some sample xml files can be parsed and then printed

---
+++ UnitTests/CommandTypesAndCLI/CLITests.cs
@@ -0,0 +1,40 @@
+﻿using CliFx;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests.CommandTypesAndCLI
+{
+    public class CLITests
+    {
+        private readonly string XmlFilePath = "XmlParsing" + Path.DirectorySeparatorChar + "TestFiles" + Path.DirectorySeparatorChar;
+        private readonly string ResultFilePath = "CommandTypesAndCLI" + Path.DirectorySeparatorChar + "TestFiles" + Path.DirectorySeparatorChar;
+
+        [Theory]
+        [InlineData("SimpleXmlTest","SimpleXmlTest")]
+        [InlineData("MultipleChildsXmlTest","MultipleChildsXmlTest")]
+        [InlineData("MultipleChildsWithDifferentChildren","MultipleChildsWithDifferentChildren")]
+        public async Task ParseXmlFiles(string xmlFileName, string resultFileName)
+        {
+            var xmlPath = Helpers.GetAbsolutePathForExternalUnitTestingFile(XmlFilePath + xmlFileName + ".xml");
+            var resultPath = Helpers.GetAbsolutePathForExternalUnitTestingFile(ResultFilePath + resultFileName + ".txt");
+
+            await using var stdOut = new MemoryStream();
+            var console = new VirtualConsole(output: stdOut);
+            
+            var app = new CliApplicationBuilder().AddCommand(typeof(MALExporter.Command)).UseConsole(console).Build();
+
+            var args = new string[] {xmlPath, "child" };
+
+            await app.RunAsync(args);
+
+            var stdOutData = console.Output.Encoding.GetString(stdOut.ToArray());
+            var expectedData = File.ReadAllText(resultPath);
+
+            stdOutData = stdOutData.Trim();
+            expectedData = expectedData.Trim();
+
+            Assert.Equal(expectedData, stdOutData);
+        }
+    }
+}
